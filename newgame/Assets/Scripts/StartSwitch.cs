07265f899fdcfd977966b50086aced3c7019f252
mrcrispy0 <125771900+mrcrispy0@users.noreply.github.com>
2023-02-19 15:02:55 -0800

    upload

---
+++ newgame/Assets/Scripts/StartSwitch.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartSwitch : MonoBehaviour
+{
+    private void switcher()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
+
