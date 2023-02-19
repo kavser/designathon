0d690575585fa1958479808a9ec0f133f92df72a
mrcrispy0 <125771900+mrcrispy0@users.noreply.github.com>
2023-02-19 13:19:05 -0800

    123

---
+++ newgame/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+
+    void Update()
+    {
+      transform.LookAt(target);
+    }  
+   
+}
